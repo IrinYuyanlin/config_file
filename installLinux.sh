de53d3360e0be6b0a4b081e15e464d4f19af19c5
IrinYuyanlin <2377463157@qq.com>
2017-06-03 23:05:21 +0800

    添加了linux系统sh和wing算号器

---
+++ installLinux.sh
@@ -0,0 +1,59 @@
+#bin/bash
+
+## update
+#sudo apt-get update;
+#
+## vim
+#sudo apt-get install vim;
+#
+## set vpn
+#sudo apt-get install network-manager-gnome network-manager-pptp network-manager-openvpn;
+#sudo /etc/init.d/network-manager restart;
+#
+# get ros-indigo
+#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list';
+#sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116;
+#sudo apt-get update;
+#sudo apt-get install ros-indigo-desktop-full;
+#sudo rosdep init;
+#rosdep update;
+#echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc;
+#source ~/.bashrc;
+#sudo apt-get install python-rosinstall;
+#
+## ros-package
+#sudo apt-get install ros-indigo-turtlebot-gazebo;
+#
+## pip
+#sudo apt-get install python-pip;
+#
+## openssh-serve
+#sudo apt-get install openssh-serve;
+#
+## jdk
+#sudo apt-get install openjdk-7-jdk;
+#sudo add-apt-repository ppa:webupd8team/java;
+#sudo apt-get update;
+#sudo apt-get install oracle-java8-installer;
+#sudo apt-get install oracle-java8-set-default;
+#
+## tmux
+#sudo apt-get install tmux;
+#
+## vundle
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#
+## vimrc
+#cd ~/download/;
+#git clone https://github.com/IrinYuyanlin/config_file.git;
+#cp config_file/vim/.vimrc ~/;
+#cd;
+
+## sougou
+#sudo apt-get-repository ppa:fcitx-team/nightly;
+## then install sougou in software center
+
+## get vim 8.0
+#sudo add-apt-repository ppa:jonathonf/vim
+#sudo apt update
+#sudo apt install vim
